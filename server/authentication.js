fbfad9916e9edb7186943f18380fb7035bb48b4f
tgermain <timothee.germain@wanadoo.fr>
2014-11-13 00:48:41 +0100

    add a server side js file whch publish userdata and presentations collections

---
+++ server/authentication.js
@@ -0,0 +1,23 @@
+// server
+Meteor.publish("userData", function() {
+    if (this.userId) {
+        return Meteor.users.find({
+            _id: this.userId
+        });
+    } else {
+        this.ready();
+    }
+});
+
+Meteor.publish("presentations", function(){
+	return Presentations.find({}, {
+		/*
+		sort: Sort specifier,
+		skip: Number,
+		limit: Number,
+		fields: Field specifier,
+		reactive: Boolean,
+		transform: Function
+		*/
+	});
+});
