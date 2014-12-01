c24ed81f3cce17abb8a66634d2a9cc8c5356b584
tgermain <timothee.germain@wanadoo.fr>
2014-12-01 17:41:40 +0100

    adding a qrcode generatore and template

---
+++ client/template/qrcode.js
@@ -0,0 +1,16 @@
+
+/*parameters  
+stringToRender : the string correspnding to this qrcode
+size : the size of the qrCode svg
+
+example of call (in html ) : 
+{{>qrcode stringToRender="This is a test" size=150}}
+*/
+Template.qrcode.rendered = function () {
+	
+	console.log(this.data);
+
+    var qrcodesvg = new Qrcodesvg(this.data.stringToRender, document.getElementById("qrcode"), this.data.size);
+
+    qrcodesvg.draw();
+};
