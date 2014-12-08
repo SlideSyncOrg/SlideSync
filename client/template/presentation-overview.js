f9eb3aaf558f97c6c962dec7c1b809a198128427
Ian Buck <ian.r.buck@gmail.com>
2014-12-08 14:24:52 +0000

    Copied starter code from a gist.

--- client/template/presentation-overview.js
+++ client/template/presentation-overview.js
@@ -69,4 +69,37 @@ Template.presentationOverview.events(
         //Prevent the page to reload
         return false;
     },
+    
+    //As found at https://gist.github.com/dariocravero/3922137
+    'change #imageUpload': function(event) {
+        console.log("Upload an image")
+        _.each(event.srcElement.files, function(file) {
+            Meteor.saveFile(file, file.name);
+        });
+    }
 });
+
+//As found at https://gist.github.com/dariocravero/3922137
+Meteor.saveFile = function(blob, name, path, type, callback) {
+  var fileReader = new FileReader(),
+    method, encoding = 'binary', type = type || 'binary';
+  switch (type) {
+    case 'text':
+      // TODO Is this needed? If we're uploading content from file, yes, but if it's from an input/textarea I think not...
+      method = 'readAsText';
+      encoding = 'utf8';
+      break;
+    case 'binary': 
+      method = 'readAsBinaryString';
+      encoding = 'binary';
+      break;
+    default:
+      method = 'readAsBinaryString';
+      encoding = 'binary';
+      break;
+  }
+  fileReader.onload = function(file) {
+    Meteor.call('saveFile', file.srcElement.result, name, path, encoding, callback);
+  }
+  fileReader[method](blob);
+}
--- server/methods.js
+++ server/methods.js
@@ -298,5 +298,39 @@ Meteor.methods(
     'hasAccessToPresentation': function(parentPresId) {
         var ownerId = Presentations.findOne({_id: parentPresId}, {ownerId : 1, _id : 0}).ownerId;
         return Meteor.userId() == ownerId;
+    },
+    
+    //As found at https://gist.github.com/dariocravero/3922137
+    'saveFile': function(blob, name, path, encoding) {
+        var path = cleanPath(path),
+            fs = __meteor_bootstrap__.require('fs'),
+            name = cleanName(name || 'file'),
+            encoding = encoding || 'binary',
+            chroot = Meteor.chroot || 'public';
+        // Clean up the path. Remove any initial and final '/' -we prefix them-,
+        // any sort of attempt to go to the parent directory '..' and any empty directories in
+        // between '/////' - which may happen after removing '..'
+        path = chroot + (path ? '/' + path + '/' : '/');
+
+        // TODO Add file existance checks, etc...
+        fs.writeFile(path + name, blob, encoding, function(err) {
+            if (err) {
+                throw (new Meteor.Error(500, 'Failed to save file.', err));
+            }
+            else {
+                console.log('The file ' + name + ' (' + encoding + ') was saved to ' + path);
+            }
+        });
+
+        function cleanPath(str) {
+            if (str) {
+                return str.replace(/\.\./g, '').replace(/\/+/g, '').
+                replace(/^\/+/, '').replace(/\/+$/, '');
+            }
+        }
+
+        function cleanName(str) {
+            return str.replace(/\.\./g, '').replace(/\//g, '');
+        }
     }
 })
