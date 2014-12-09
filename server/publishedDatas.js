de4e088b44da5d2f7e655054902ac14f600c6b54
tgermain <timothee.germain@wanadoo.fr>
2014-12-09 01:19:29 +0100

    formating

--- server/publishedDatas.js
+++ server/publishedDatas.js
@@ -1,47 +1,32 @@
 // server
-Meteor.publish("userData", function()
-{
-    if (this.userId)
-    {
-        return Meteor.users.find(
-        {
+Meteor.publish("userData", function() {
+    if (this.userId) {
+        return Meteor.users.find({
             _id: this.userId
         });
-    }
-    else
-    {
+    } else {
         this.ready();
     }
 });
 
-Meteor.publish("presentations", function()
-{
-    if (this.userId)
-    {
-        return Presentations.find(
-        {
+Meteor.publish("presentations", function() {
+    if (this.userId) {
+        return Presentations.find({
             //only get data the user own
             'ownerId': this.userId
         });
-    }
-    else
-    {
+    } else {
         this.ready();
     }
 });
 
-Meteor.publish("slides", function()
-{
-    if (this.userId)
-    {
-        return Slides.find(
-        {
+Meteor.publish("slides", function() {
+    if (this.userId) {
+        return Slides.find({
             //only get our data
-            'ownerId': this.userId
+            'ownerId': this.userId,
         });
-    }
-    else
-    {
+    } else {
         this.ready();
     }
 });
