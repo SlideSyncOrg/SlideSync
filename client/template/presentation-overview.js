3f7a7d76e42c41f23e07bab389a2aa4c8af679ae
tgermain <timothee.germain@wanadoo.fr>
2014-12-09 01:22:31 +0100

    formatting

--- client/template/presentation-overview.js
+++ client/template/presentation-overview.js
@@ -1,29 +1,23 @@
-Template.presentationOverview.created = function()
-{
+Template.presentationOverview.created = function() {
     //When the template is created, store the presentation id in session
     Session.set("thePrez", this.data._id);
 };
 
 
-Template.presentationOverview.helpers(
-{
-    'gimmeStatesArray': function()
-    {
+Template.presentationOverview.helpers({
+    'gimmeStatesArray': function() {
         //Generate an array of sized statesCount containing [1,2,3,...]
         var N = this.statesCount;
         var res = [];
-        for (var i = 1; i <= N; i++)
-        {
-            res.push(
-            {
+        for (var i = 1; i <= N; i++) {
+            res.push({
                 index: i
             });
         }
         return res;
     },
 
-    'displayStateCount': function()
-    {
+    'displayStateCount': function() {
 
         return this.statesCount;
     },
@@ -37,7 +31,7 @@ Template.presentationOverview.helpers(
         }).content;
     },
 
-    'log':function(){
+    'log': function() {
         console.log(this)
     },
 
@@ -53,17 +47,14 @@ Template.presentationOverview.helpers(
 });
 
 
-Template.presentationOverview.events(
-{
+Template.presentationOverview.events({
     //Add a new state to this presentation
-    'click .addState': function(event)
-    {
+    'click .addState': function(event) {
         Meteor.call('addState', Session.get('thePrez'));
     },
 
     //Add a new timeline to this presentation
-    'submit #addTimeline': function(event)
-    {
+    'submit #addTimeline': function(event) {
         var title = event.target.title.value;
         var isPublic = event.target.isPublic.checked;
 
