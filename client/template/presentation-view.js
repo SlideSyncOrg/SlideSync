132116173130334c1270c4414a1c17b5d3c346bb
tgermain <timothee.germain@wanadoo.fr>
2014-12-10 17:34:10 +0100

    adapte the presentation.view to changes in slide content

--- client/template/presentation-view.js
+++ client/template/presentation-view.js
@@ -1,14 +1,11 @@
-Template.presentationView.created = function()
-{
+Template.presentationView.created = function() {
     Session.set("sideBarHided", false);
 
     //When the template is created, store the presentation object in session
     Session.set("thePrez", this.data.presentation);
 
-    document.addEventListener('keydown', function(event)
-    {
-        switch (event.keyCode)
-        {
+    document.addEventListener('keydown', function(event) {
+        switch (event.keyCode) {
             case 39: // Right arrow
                 event.preventDefault();
                 Meteor.call('nextState', Session.get('thePrez')._id);
@@ -21,93 +18,100 @@ Template.presentationView.created = function()
     }, false);
 };
 
-Template.presentationView.helpers(
-{
-    'currentTimeline': function()
-    {
+Template.presentationView.helpers({
+    'currentTimeline': function() {
         return this.presentation.timelines[this.timelineIndex].titleToDisplay;
     },
 
-    'currentState': function()
-    {
+    'currentState': function() {
         //Return state;
         return this.presentation.currentState;
     },
 
-    'currentContent': function()
-    {
+    'currentContent': function() {
         var content = "We got nothing";
         var timelineTitle = this.presentation.timelines[this.timelineIndex].title;
         var stateNumber = this.presentation.currentState;
 
-/*        //Go through slides array and find the one with current timeline and state
-        this.presentation.slides.forEach(function(slide)
-        {
-            if (slide.timeline == timelineTitle && slide.state == stateNumber)
-            {
-                content = slide.content;
-            }
+        /*        //Go through slides array and find the one with current timeline and state
+                this.presentation.slides.forEach(function(slide)
+                {
+                    if (slide.timeline == timelineTitle && slide.state == stateNumber)
+                    {
+                        content = slide.content;
+                    }
+                });
+
+                return content;*/
+
+        daSlide = Slides.findOne({
+            'parentPresId': Session.get("thePrez")._id,
+            'timeline': timelineTitle,
+            'state': stateNumber,
         });
 
-        return content;*/
+        if (daSlide.isHtml) {
+            return daSlide.content
+        } else {
+            daImage = Images.findOne({
+                '_id': daSlide.content
+            });
+            console.log(daImage);
+            return daImage;
+        };
 
-        // return Slides.findOne(
-        // {
-        //     'parentPresId': Session.get("thePrez")._id,
-        //     'timeline': timelineTitle,
-        //     'state': stateNumber,
-        // }).content;
-        
-        return Images.findOne({
-            '_id': "ZdzvmME4LSRpfisMA"
-        }).data.name;
+        // return Images.findOne({
+        //     '_id': "ZdzvmME4LSRpfisMA"
+        // }).data.name;
     },
 
-    'isOwner': function()
-    {
+    'slideIsHtml': function() {
+        var timelineTitle = this.presentation.timelines[this.timelineIndex].title;
+        var stateNumber = this.presentation.currentState;
+        return Slides.findOne({
+            'parentPresId': Session.get("thePrez")._id,
+            'timeline': timelineTitle,
+            'state': stateNumber,
+        }).isHtml;
+    },
+
+    'isOwner': function() {
         return Session.get('thePrez').ownerId === Meteor.userId();
     },
 
-    'owner': function()
-    {
+    'owner': function() {
         return Session.get('thePrez').owner;
     },
 
-    'isAuthorized': function(currentTimelineIndex)
-    {
+    'isAuthorized': function(currentTimelineIndex) {
         return Session.get('thePrez').ownerId === Meteor.userId() || Session.get('thePrez').timelines[currentTimelineIndex].isPublic;
     },
 
-    'isSidebarToggled': function()
-    {
+    'isSidebarToggled': function() {
         return Session.get("sideBarHided");
     },
 
 
     'getUrlData': function() {
         return {
-            _id:Session.get("thePrez")._id,
-            timeline:this.index
+            _id: Session.get("thePrez")._id,
+            timeline: this.index
         }
     },
 
 
 });
 
-Template.presentationView.events(
-{
-    'click #nextState': function(event)
-    {
+Template.presentationView.events({
+    'click #nextState': function(event) {
         Meteor.call('nextState', this.presentation._id);
     },
 
-    'click #previousState': function(event)
-    {
+    'click #previousState': function(event) {
         Meteor.call('previousState', this.presentation._id);
     },
 
-    'click #toggleSidebar': function(event)
-    {
+    'click #toggleSidebar': function(event) {
         $("#wrapper").toggleClass("toggled");
         Session.set("sideBarHided", !Session.get("sideBarHided"));
     },
