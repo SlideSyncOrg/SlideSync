25174d7afd2399370e3e191a61db5a3d4540df81
tgermain <timothee.germain@wanadoo.fr>
2014-12-10 17:33:46 +0100

    two editors : - htmlEditor - image uploader
    and you choose what is the content of the slide (image/html)

--- client/template/slide-edit.js
+++ client/template/slide-edit.js
@@ -1,19 +1,113 @@
-Template.slideEdit.helpers(
-{
-    'log': function()
-    {
+Template.slideEdit.created = function() {
+    //true -> html editor
+    //false -> image uploader
+    Session.set("currentEditor", true);
+}
+
+Template.slideEdit.helpers({
+    'log': function() {
         console.log(this)
     },
-    'trimedContent':function() {
-    	return this.content.trim()
-    }
+
+
+    'currentEditor': function() {
+        return Session.get("currentEditor");
+    },
+
+
 });
 
-Template.slideEdit.events(
-{
-    'click #submitChanges': function(event)
-    {
-        newContent = document.getElementById("textAreaContent").value
-        Meteor.call('updateSlideContent', this.parentPresId,this.timeline,this.state,newContent);
+Template.slideEdit.events({
+
+
+    'click #toggleEditor': function() {
+        console.log("Toggling the editor mode current mode", Session.get("currentEditor"))
+            // this.isHtml = !this.isHtml
+        Session.set("currentEditor", !Session.get("currentEditor"));
     },
+
+
+});
+
+
+/*--------------------------Image uploader code*/
+Template.imageUploader.helpers({
+    'randImg': function() {
+        return Images.findOne();
+    },
+
+    'imgs': function() {
+        res = Images.find();
+        console.log(res.fetch());
+        return res
+    },
+});
+
+Template.imageUploader.events({
+    //As found at https://gist.github.com/dariocravero/3922137
+    'change #imageUpload': function(event, template) {
+        //console.log(event);
+        FS.Utility.eachFile(event, function(file) {
+            var newFile = new FS.File(file);
+            Images.insert(newFile, function(err, fileObj) {
+                console.log(err);
+                console.log(fileObj);
+                //If !err, we have inserted new doc with ID fileObj._id, and
+                //kicked off the data upload using HTTP
+            });
+
+        });
+    },
+
+    'click .anImage': function() {
+        console.log(this)
+        Session.set("selectedImage", this._id);
+    },
+
+    'click #submitNewContent': function() {
+        console.log("selected image",Session.get("selectedImage"))
+        console.log("type of slide  true -> html editor false -> image uploader", Session.get("currentEditor"))
+
+
+
+
+        // image uploader
+        Meteor.call('updateSlideContent', this.parentPresId, this.timeline, this.state, Session.get("currentEditor"), Session.get("selectedImage"));
+
+    },
+});
+
+
+/*--------------------------html editor code*/
+Template.htmlEditor.helpers({
+    'getEditorContent': function() {
+        // console.log(document.getElementById("textAreaContent"))
+
+        // currentContent = document.getElementById("textAreaContent")
+        // console.log("what we get from text area", currentContent)
+        // if (currentContent == "") {
+        return this.content;
+        // } else {
+        //     return currentContent
+        // }
+    },
+
+    'trimedContent': function() {
+        return this.content.trim()
+    },
+});
+Template.htmlEditor.events({
+
+    'click #previewChanges': function(event) {
+        newContent = document.getElementById("textAreaContent").value
+            // Meteor.call('updateSlideContent', this.parentPresId, this.timeline, this.state, newContent);
+    },
+
+    'click #submitNewContent': function() {
+        //html editor
+        newContent = document.getElementById("textAreaContent").value
+        Meteor.call('updateSlideContent', this.parentPresId, this.timeline, this.state, Session.get("currentEditor"), newContent);
+
+    },
+
 });
