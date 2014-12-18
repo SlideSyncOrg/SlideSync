0c9538261a6f9b035256a5f62b6e7463c68a752a
tgermain <timothee.germain@wanadoo.fr>
2014-12-18 16:52:06 +0100

    comment on origin of snippet

--- client/utils/indexArray.js
+++ client/utils/indexArray.js
@@ -1,3 +1,5 @@
+// from http://stackoverflow.com/questions/24225071/how-can-i-get-the-index-of-an-array-in-a-meteor-template-each-loop
+
 UI.registerHelper('addIndex', function (all) {
     return _.map(all, function(val, index) {
         return {index: index, value: val};
